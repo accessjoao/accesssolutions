e911a0561c2ba8e267ed049f8fef1df11e08fcf1
Joao Felipe Silveira <joaofe747@gmail.com>
2022-11-28 11:00:02 -0500

    clients section complete

--- client/src/components/CardDeal.jsx
+++ client/src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-export default CardDeal
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better payment solution in few<br className="sm:block hidden" />  easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -40,21 +40,21 @@ export const features = [{
 
 export const feedback = [{
         id: "feedback-1",
-        content: "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+        content: "The team was very supportive and kept me motivated. This was one of the best investments I've made in my company.",
         name: "Herman Jensen",
         title: "Founder & Leader",
         img: people01,
     },
     {
         id: "feedback-2",
-        content: "Money makes your life easier. If you're lucky to have it, you're lucky.",
+        content: "Such a life-changing experience. Highly recommended!",
         name: "Steve Mark",
         title: "Founder & Leader",
         img: people02,
     },
     {
         id: "feedback-3",
-        content: "It is usually people in the money business, finance, and international trade that are really rich.",
+        content: "An overall wonderful and rewarding experience.",
         name: "Kenn Gallagher",
         title: "Founder & Leader",
         img: people03,
